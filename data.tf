84af8e277dff83bc3add054b3357e46f35f7eb43
balatuluva <bala.tulva@gmail.com>
2025-03-10 06:57:14 +0000

    rds mysql setup correction

--- data.tf
+++ data.tf
@@ -8,8 +8,8 @@ data "aws_ssm_parameter" "db_pass" {
 }
 
 data "aws_subnet" "RDS-VPC-Public-Subnet" {
-  id = "subnet-0e5122f43ac79a246"
   name = "RDS-VPC-Public-Subnet"
+  id = "subnet-0e5122f43ac79a246"
 }
 
 data "aws_security_group" "RDS-SG" {
