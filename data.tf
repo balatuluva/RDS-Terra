e12428856f66e928aa704768c18ff68ae6a5f66c
balatuluva <83805182+balatuluva@users.noreply.github.com>
2025-03-10 09:34:18 +0000

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,12 +1,3 @@
-# not sure to use parameter store or secrets manager?
-data "aws_ssm_parameter" "db_user" {
-  name = "db_user"
-}
-
-data "aws_ssm_parameter" "db_pass" {
-  name = "db_pass"
-}
-
 data "aws_subnet" "RDS-VPC-Public-Subnet" {
   id = "subnet-0e5122f43ac79a246"
 }
