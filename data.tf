db6edb03d4f396a9aac91b875c5d8b9f551edc07
balatuluva <bala.tulva@gmail.com>
2025-04-17 10:52:16 +0100

    meta data append

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
-data "aws_subnet" "RDS-VPC-Public-Subnet" {
-  id = "subnet-035c48b8affd56fef"
-}
-
-data "aws_security_group" "RDS-SG" {
-  id = "sg-04020f8ef6ed43e13"
-}
+# data "aws_subnet" "RDS-VPC-Public-Subnet" {
+#   id = "subnet-035c48b8affd56fef"
+# }
+#
+# data "aws_security_group" "RDS-SG" {
+#   id = "sg-04020f8ef6ed43e13"
+# }
