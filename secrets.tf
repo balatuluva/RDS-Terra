c8ed3ec92cc4302a4f01904d6181484ee6205508
balatuluva <bala.tulva@gmail.com>
2025-04-23 16:22:42 +0100

    secret correction

--- secrets.tf
+++ secrets.tf
@@ -4,12 +4,12 @@ resource "random_password" "db_password" {
   override_special = "_!%^"
 }
 
-resource "aws_secretsmanager_secret" "db_secret" {
-  name = "db-password"
+resource "aws_secretsmanager_secret" "db_secret_password" {
+  name = "db-secret-password"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_version" {
   secret_string = random_password.db_password.result
-  secret_id           = aws_secretsmanager_secret.db_secret.id
+  secret_id           = aws_secretsmanager_secret.db_secret_password.id
   #secret_name = aws_secretsmanager_secret.db_secret.name
 }
